7e3cd7a8180c27d6c769f8255a81fae3984ad6e2
romanbelopolski <roman.belopolski@gmail.com>
2025-12-18 15:48:47 +0300

    HT14

---
+++ script2.js
@@ -0,0 +1,91 @@
+class ObjStorageClass{
+    
+    constructor(){
+    this.drinks ={}}
+
+
+
+    addValue(key,value){
+        this.drinks[key]=value;
+    };
+
+    getValue(key){
+       return this.drinks[key]
+    };
+
+    deleteValue(key){
+        let arr = Object.keys(this.drinks);
+        let value = key;
+        delete this.drinks[key]
+        if(arr.includes(value)){
+            return true
+        }
+        else{
+            return false
+        }
+   }
+
+
+    getKeys(){
+     return  Object.keys(this.drinks);
+    };
+}
+
+
+
+const drinkStorage = new ObjStorageClass()
+
+
+function setValue() {
+  let drinkname,alcohol,receipt;
+  drinkname = prompt("Введите название напитка");
+  if(drinkname !==null && drinkname !==""){
+  alcohol = confirm("ОК - если напиток алкогольный,Отмена - если напиток не алкогольный");
+  do { receipt = prompt("Введите рецепт напитка")
+  } while(receipt==null || receipt=="");
+
+  drinkStorage.addValue(drinkname,{alcohol,receipt});
+}
+  
+}
+
+function readValue(){
+   let listValue = drinkStorage.getKeys();
+   if(listValue.length==0){
+    return alert("Напитков нет")
+   }
+   else{return alert(listValue)}
+}
+
+function getValue(){
+    let key = prompt("Введите название напитка")
+    if (key !==null&& key!==''){
+    let descriptionDrink = drinkStorage.getValue(key);
+    let alc;
+    if (descriptionDrink.alcohol){
+        alc = "алкогольный"
+    }
+    else{
+        alc = 'не алкогольный'
+    }
+    let text = 
+`напиток: ${key}
+алкогольный: ${alc}
+рецепт приготовления: ${descriptionDrink.receipt}`
+    return alert(text)}
+}
+
+function delValue(){
+    let key;
+    key = prompt("Введите название напитка");
+
+    if(key!==null && key !==''){
+    let messageValue = drinkStorage.deleteValue(key);
+    if(messageValue){
+        return alert("Напиток удален")
+    }
+    else{
+        return alert("такого напитка нету в системе")
+    }
+}
+}
